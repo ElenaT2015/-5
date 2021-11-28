57914c2317a29d07f2abaa5c5ed1580cf370d523
Tsygankovae.a <Tsygankovae.a@outlook.com>
2021-11-28 21:04:50 +0300

    Добавьте файлы проекта.

---
+++ Задание 5.5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_5._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество  строк и столбцов массива");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[N, N];
+            Random random = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    array[i, j] = random.Next(0, 2);
+                    Console.Write(" {0,2} " , array[i ,j ]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задание 5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 7 элементов массива");
+            const int  n  = 7;
+            int[] array = new int[n];
+            double S = 0;
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                S += array[i];         
+            }
+            Console.WriteLine(" Среднее арифмитческое значение = {0:f3}", S / n);
+            Console.ReadKey();
+        }
+    }
+}
